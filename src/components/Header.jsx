79162cc7aa93eb570b070edad8f0c29e4f58986e
XX <fallwall19@gmail.com>
2020-10-07 18:43:12 -0700

    nav setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">TIBBY XU</div>
+      <div className="header-right">
+        <div>ABOUT</div>
+        <div>PROJECTS</div>
+        <div>CONTACT</div>
+      </div>
+    </div>
+  )
+}
